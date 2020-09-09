0032b9b9aeb7b516f92ad3f5078b8fd71d0d42e2
Devin Cooper <dcoope12@asu.edu>
2020-09-09 16:25:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   squared_array = []
-  array.each do |i|
-      square = i ** 2
-      square_array << square
-    end
-    squared_array
+  array.each do |num|
+    square = num ** 2
+    squared_array << square
+  end
+  squared_array
 end
